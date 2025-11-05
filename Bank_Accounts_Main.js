8ea498c3c51c0259f4322b0927e4b44d81937f88
Amit <omamitrashmi@gmail.com>
2025-11-05 14:40:42 -0400

    Update Bank_Accounts_Main.js

--- Bank_Accounts_Main.js
+++ Bank_Accounts_Main.js
@@ -866,9 +866,7 @@ document.addEventListener("DOMContentLoaded", () => {
           <div class="notes-card-minimal">
           <div class="section-heading-minimal"><strong>Nomination</strong></div>
           <div class="notes-content-minimal">
-            <div><strong>Name:</strong> ${record.Bank_Nominee_Name_Text || record.Bank_Nominee_Name || ''}</div>
-            <div><strong>Email:</strong> ${record.Bank_Nominee_Email || ''}</div>
-            <div><strong>Phone:</strong> ${record.Bank_Nominee_Phone || ''}</div>
+            <div><strong>Name:</strong> ${record.Bank_Nominee_Name_Text || record.Bank_Nominee_Name || ''} | <strong>Email:</strong> ${record.Bank_Nominee_Email || ''} | <strong>Phone:</strong> ${record.Bank_Nominee_Phone || ''}</div>
           </div>
         </div>
       </div>
